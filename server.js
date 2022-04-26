be5348fe479979405689fc55e3b6081201e1a249
Rakesh Potnuru <rakeshpotnuru321@gmail.com>
2022-04-26 19:55:03 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/todoapiDB";
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    app.listen(PORT, console.log("Server stated on port 5000"));
+  })
+  .catch((err) => {
+    console.log(err);
+  });
