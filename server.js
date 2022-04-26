b007217eafc0c54e159fc2ef6064a79e6b230e8f
Rakesh Potnuru <rakeshpotnuru321@gmail.com>
2022-04-26 21:10:08 +0530

    add task model
    Signed-off-by: Rakesh Potnuru <rakeshpotnuru321@gmail.com>

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const Task = require("./models/tasks.model");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/todoapiDB";
