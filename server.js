7915108a543b5c2051419411c1addd1279a9b0bd
Rakesh Potnuru <rakeshpotnuru321@gmail.com>
2022-04-26 23:39:00 +0530

    Add requests
    Signed-off-by: Rakesh Potnuru <rakeshpotnuru321@gmail.com>

--- server.js
+++ server.js
@@ -13,6 +13,49 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// get all tasks
+app.get("/getTasks", async (req, res) => {
+  try {
+    const response = await Task.find();
+    res.json(response);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// post a task
+app.post("/postTask", async (req, res) => {
+  try {
+    const response = await Task.create(req.body);
+    res.json(response);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// delete a task
+app.delete("/deleteTask/:id", async (req, res) => {
+  try {
+    const response = await Task.deleteOne({ _id: req.params.id });
+    res.json(response);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// edit a task
+app.patch("/editTask/:id", async (req, res) => {
+  try {
+    const response = await Task.updateOne(
+      { _id: req.params.id },
+      { $set: req.body }
+    );
+    res.json(response);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true })
   .then(() => {
